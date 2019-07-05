9c242b014226c7f256118666296a4322b3cf7e70
Amar <amarsgithub@outlook.com>
2019-07-05 11:18:36 -0400

    Isomorphic strings, try t

--- Week 2 - hashmaps, sets, multisets, tries/205. Isomorphic Strings/main.cpp
+++ Week 2 - hashmaps, sets, multisets, tries/205. Isomorphic Strings/main.cpp
@@ -5,30 +5,17 @@ using namespace std;
 bool isIsomorphic(string s, string t)
 {
 
-    if (s.size() == 0) 
-        return true;
+    int m1[128] = {0};
+    int m2[128] = {0};
 
-    unordered_map<char, char> sMap;
-    unordered_map<char, char> tMap;
-
-    sMap.insert(make_pair(s[0], t[0]));
-    sMap.insert(make_pair(t[0], s[0]));
-
-    for (int i = 1; i < s.length(); i++) 
+    for (int i = 0; i < s.length(); i++)
     {
-        if (sMap.find(s[i]) != sMap.end())
-            if (sMap.at(s[i]) != t[i])
-                return false;
-
-        if (tMap.find(t[i]) != tMap.end())
-            if (tMap.at(t[i]) != s[i])
-                return false;
-
-        sMap.insert(make_pair(s[i], t[i]));
-        sMap.insert(make_pair(t[i], s[i]));
+        if (m1[s[i]] != m2[t[i]])
+            return false;
+        m1[s[i]] = i + 1;
+        m2[t[i]] = i + 1;
     }
     return true;
-
 }
 
 int main(void)
