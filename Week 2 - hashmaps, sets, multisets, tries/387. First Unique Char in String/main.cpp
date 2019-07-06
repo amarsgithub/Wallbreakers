05189b07a78db9bb177a8713d9fccdefca8eaa8d
Amar <amarsgithub@outlook.com>
2019-07-06 16:14:26 -0400

    Redid first unique character... Used a vector instead

--- Week 2 - hashmaps, sets, multisets, tries/387. First Unique Char in String/main.cpp
+++ Week 2 - hashmaps, sets, multisets, tries/387. First Unique Char in String/main.cpp
@@ -4,25 +4,15 @@ public:
     int firstUniqChar(string s)
     {
 
-        if (s.length() == 0)
-            return -1;
-        if (s.length() == 1)
-            return 0;
+        vector<int> vec(26, 0);
 
         for (int i = 0; i < s.length(); i++)
-        {
-            bool isRepeated = false;
-            for (int j = 0; j < s.length(); j++)
-            {
-                if (i != j && s[i] == s[j])
-                {
-                    isRepeated = true;
-                    break;
-                }
-            }
-            if (!isRepeated)
+            vec[s[i] - 'a']++; // Kind of like a hashmap, storing the frequencies of each value
+
+        for (int i = 0; i < s.length(); i++)
+            if (vec[s[i] - 'a'] == 1) // If the character only occurs once, then just return that character
                 return i;
-        }
+
         return -1;
     }
 };
